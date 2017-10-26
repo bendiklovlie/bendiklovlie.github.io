0db1c0acd545deb141988311928f17211ef939f0
Bendik Løvlie <bendiklovlie@Bendiks-MacBook-Pro.local>
2017-10-26 14:58:09 +0200

    hg

--- tegneblokk/tegneblokk.js
+++ tegneblokk/tegneblokk.js
@@ -1,13 +1,63 @@
-function setup(){
+function setup() {
+
+    let xpos = 0;
+    let ypos = 0;
+
 
     let firkantDiv = document.getElementById("firkant");
-    let trekantDiv = document.getElementById("firkant");
-    let sirkelDiv = document.getElementById("firkant");
-    let restartDiv = document.getElementById("firkant");
-    let rodDiv = document.getElementById("firkant");
-    let gronnDiv = document.getElementById("firkant");
-    let gulDiv = document.getElementById("firkant");
-    let blaDiv = document.getElementById("firkant");
+    firkantDiv.addEventListener("click", lagFirkant);
+
+    let trekantDiv = document.getElementById("trekant");
+    trekantDiv.addEventListener("click", lagTrekant);
+
+    let sirkelDiv = document.getElementById("sirkel");
+    sirkelDiv.addEventListener("click", lagSirkel);
+
+    let restartDiv = document.getElementById("restart");
+    restartDiv.addEventListener("click", restartFunksjon);
+
+    let rodDiv = document.getElementById("rod");
+    let gronnDiv = document.getElementById("gronn");
+    let gulDiv = document.getElementById("gul");
+    let blaDiv = document.getElementById("bla");
+
+    let canvas = document.getElementById("tegning");
+    let ctx = canvas.getContext('2d');
+
+    let type = "";
+
+    canvas.addEventListener("click", posisjon);
+
+    function posisjon(e) {
+        xpos = e.clientX - 140;
+        ypos = e.clientY - 10;
+        console.log(xpos + " " + ypos);
+        switch (type) {
+            case "firkant":
+                tegnFirkant();
+                break;
+        }
+
+    }
+    function lagFirkant(event) {
+        type = "firkant";
+    }
+
+    function tegnFirkant() {
+        ctx.beginPath();
+        ctx.moveTo(xpos, ypos);
+        ctx.lineTo(xpos, ypos + 50);
+        ctx.lineTo(xpos + 50, ypos + 50);
+        ctx.lineTo(xpos + 50, ypos);
+        ctx.lineTo(xpos, ypos);
+        ctx.stroke();
+    }
+    function lagTrekant(event) {
+
+    }
+    function lagSirkel(event) {
+
+    }
 
 
 
@@ -16,10 +66,7 @@ function setup(){
 
 
 
-    let restartKnapp = document.getElementById("restart");
-    restartKnapp.addEventListener("click",restartFunksjon);
-
-    function restartFunksjon(event){
+    function restartFunksjon(event) {
         location.reload();
     }
 
